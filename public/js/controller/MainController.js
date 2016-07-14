844c3b665e2f663caae1c0ef7b4709dce127383f
Daniel <danielparra93@gmail.com>
2016-07-14 19:25:14 -0400

    get,create,and delete work

--- public/js/controller/MainController.js
+++ public/js/controller/MainController.js
@@ -7,21 +7,54 @@
 
   function MainController($scope, BearService){
     // console.log('Main!');
-    $scope.message = 'Hey now! What that is sound?';
-    $scope.messages = 'some more stuff';
-    var msg = 'a non scoped messaged';
+    // $scope.message = 'Hey now! What that is sound?';
+    // $scope.messages = 'some more stuff';
+    // var msg = 'a non scoped messaged';
     // console.log(BearService);
+    $scope.bears = BearService.bears;
+    $scope.create = createBear;
+    $scope.delete = deleteBear;
+    getBears();
 
+    // var bears;
+    // BearService.readAll()
+    //             .then(function(){
+    //               bears = BearService.bears;
+    //               console.log(bears);
+    //             });
+    // BearService.create();
+    // BearService.delete();
+    // BearService.update();
 
-    var bears;
-    BearService.readAll()
-                .then(function(){
-                  bears = BearService.bears;
-                  console.log(bears);
-                });
-    BearService.create();
-    BearService.delete();
-    BearService.update();
+    function getBears(){
+      BearService.readAll()
+                  .then(function(){
+                    $scope.bears = BearService.bears;
+                    // console.log($scope.bears);
+                  })
+    }
+
+    function createBear(size,color){
+      // if(isEmpty(size) || isEmpty(color){
+      //   getBears();
+      // } else{
+
+      BearService.create(size,color)
+                  .then(function(){
+                    $scope.size = '';
+                    $scope.color = '';
+                    getBears();
+                  })
+        // }
+    }
+
+    function deleteBear(id){
+      console.log(id);
+      BearService.delete(id)
+                  .then(function(){
+                    getBears();
+                  })
+    }
 
   }
 })();
