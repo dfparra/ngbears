e88258e2641198289819ef3f6b11917b39a22329
Daniel <danielparra93@gmail.com>
2016-07-15 18:17:28 -0400

    cleaned up code

--- public/js/controller/MainController.js
+++ public/js/controller/MainController.js
@@ -6,25 +6,10 @@
   MainController.$inject = ['$scope', 'BearService'];
 
   function MainController($scope, BearService){
-    // console.log('Main!');
-    // $scope.message = 'Hey now! What that is sound?';
-    // $scope.messages = 'some more stuff';
-    // var msg = 'a non scoped messaged';
-    // console.log(BearService);
     $scope.bears = BearService.bears;
     $scope.create = createBear;
     $scope.delete = deleteBear;
     getBears();
-
-    // var bears;
-    // BearService.readAll()
-    //             .then(function(){
-    //               bears = BearService.bears;
-    //               console.log(bears);
-    //             });
-    // BearService.create();
-    // BearService.delete();
-    // BearService.update();
 
     function getBears(){
       BearService.readAll()
@@ -35,10 +20,6 @@
     }
 
     function createBear(size,color){
-      // if(isEmpty(size) || isEmpty(color){
-      //   getBears();
-      // } else{
-
       BearService.create(size,color)
                   .then(function(){
                     $scope.size = '';
